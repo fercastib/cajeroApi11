8308819409732f1a54b92731080012812358b5eb
Fernanda Castiblanco <fernandacastiblanco95@gmail.com>
2020-12-11 14:57:03 -0500

    arreglo

--- main.py
+++ main.py
@@ -3,11 +3,10 @@ from db.user_db import update_user, get_user
 from db.transaction_db import TransactionInDB
 from db.transaction_db import save_transaction
 from models.user_models import UserIn, UserOut
-from models.transaction_models import TransactionIn,TransactionOut
-
+from models.transaction_models import TransactionIn, TransactionOut
 import datetime
-from fastapi import FastAPI
-from fastapi import HTTPException
+from fastapi import FastAPI, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
 
 api = FastAPI()
 
